53d47137cd53e126e70c5ccdac8f94272952ff3f
GIRISHGHUGARE <girishghugare19@gmail.com>
2024-12-11 16:21:07 +0530

    Routes, SignUp/LoginPage Added with some features!

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';  // Import GoogleOAuthProvider
 
+// const clientId = import.meta.env.GOOGLE_CLIENT_ID; // Replace with your actual Google Client ID
+const clientId = "768682582810-0cj57ttmiubm9fjnhqtnle277cc9best.apps.googleusercontent.com";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={clientId}>  {/* Wrap the app inside GoogleOAuthProvider */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </StrictMode>,
-)
+);

b9f88ab0e48427ccf7deeb6a154228fee479f81c
GIRISHGHUGARE <girishghugare19@gmail.com>
2024-12-12 16:21:44 +0530

    Completion of project!

--- backend/server.js
+++ backend/server.js
@@ -176,7 +176,7 @@ app.post('/api/knn/predict', (req, res) => {
     }
 });
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "client/dist")));
+    app.use(express.static(path.join(__dirname, "frontend/dist")));
 
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
